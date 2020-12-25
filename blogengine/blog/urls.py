19a351db8925bce6930ef9c3a4d00dcfa9c485a9
RowdyKGZ <RowdyKG@gmail.com>
2020-12-25 06:08:32 +0600

    Сделал до 9 урока Олег Молчанов

--- blogengine/blog/urls.py
+++ blogengine/blog/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import posts_list
+from .views import posts_list, PostDetail, tags_list, TagDetail, \
+    TagCreate, PostCreate
 
 urlpatterns = [
-    path('', posts_list),
+    path('', posts_list, name='posts_list_url'),
+    path('post/create/', PostCreate.as_view(), name='post_create_url'),
+    path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),
+    path('tags/', tags_list, name='tags_list_url'),
+    path('tag/create/', TagCreate.as_view(), name='tag_create_url'),
+    path('tag/<str:slug>/', TagDetail.as_view(), name='tag_detail_url'),
 ]
--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,6 +1,40 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import View
+
+from .models import Post, Tag
+from .utils import ObjectDetailMixin, ObjectCreateMixin
+from .forms import TagForm, PostForm
 
 
 def posts_list(request):
-    return HttpResponse('<h1>Hello world</h1>')
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+
+class PostCreate(ObjectCreateMixin, View):
+    form_model = PostForm
+    template = 'blog/post_create_form.html'
+
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
+
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
+    def get(self, request, slug):
+        tag = get_object_or_404(Tag, slug__iexact=slug)
+        return render(request, 'blog/tag_detail.html', context={'tag': tag})
+
+
+class TagCreate(ObjectCreateMixin, View):
+    form_model = TagForm
+    template = 'blog/tag_create.html'
